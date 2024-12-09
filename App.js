6a55195dc1563e0b000d2286c90e86fa6ef26e8d
yashaswi-03 <yashaswiboga@gmail.com>
2024-12-09 18:34:45 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+    res.send('Hello, Bye!');
 });
 
 app.listen(3000, '0.0.0.0', () => {
