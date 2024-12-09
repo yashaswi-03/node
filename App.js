bfc1636c4e75bca3144aa768307b57aa98ed4c0f
bogi <yashaswiboga@gmail.com>
2024-12-09 18:19:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Docker!');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
+});
